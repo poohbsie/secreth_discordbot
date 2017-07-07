5824690f5010525c3f615102ae20e93ccfaf9943
James Shiffer <jamesnshiffer@gmail.com>
2017-07-07 14:05:30 -0700

    Add example Config.py, stats.json files

---
+++ scripts/Config_Example.py
@@ -0,0 +1,9 @@
+# RENAME THIS FILE TO "Config.py" ONCE YOU ARE DONE!
+
+# TOKEN: The bot's Discord API token. Get one at https://discordapp.com/developers/applications/me/create
+# ADMIN: Your Discord user ID.
+# PREFIX: The prefix for commands. Should not contain spaces.
+
+TOKEN = ""
+ADMIN = "000000000000000000"
+PREFIX = "sh?"
